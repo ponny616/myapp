4c06abef1c1d7d7b497c7586c57d523c06286604
Limin Wang <ponny@Limins-MBP.home>
2015-06-19 02:52:49 -0400

    updates

--- client/about.js
+++ client/about.js
@@ -1,7 +1,6 @@
 var myTeam =
   [
     {name:"Limin Wang"    , email: "ponny616@brandeis.edu" },
-    {name:"xxx" , email: "xxx@brandeis.edu" }
   ];
 
 Template.about.helpers(
---
+++ client/welcom.js
@@ -0,0 +1,5 @@
+var jumboHeight = $('.jumbotron').outerHeight();
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+}
